8dafdcd30ea94bc22f4b1f9d02be5efd5da812a6
github-action-benchmark <github@users.noreply.github.com>
2022-09-07 17:05:20 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 212a898a397f954ddaa0f317e36b45e1496b9ca4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662570317381,
+  "lastUpdate": 1662570320484,
   "repoUrl": "https://github.com/bistapf/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -291,6 +291,43 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bstapf@nikhef.nl",
+            "name": "Birgit Stapf"
+          },
+          "committer": {
+            "email": "bstapf@nikhef.nl",
+            "name": "Birgit Stapf"
+          },
+          "distinct": true,
+          "id": "212a898a397f954ddaa0f317e36b45e1496b9ca4",
+          "message": "add bbtautau valid plots",
+          "timestamp": "2022-09-07T18:49:01+02:00",
+          "tree_id": "de3aa87bb42ef0b62761de6a2863ca9c0fdaa026",
+          "url": "https://github.com/bistapf/FCCAnalyses/commit/212a898a397f954ddaa0f317e36b45e1496b9ca4"
+        },
+        "date": 1662570319061,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 5.588580136515893,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 4.103688565097346,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test",
+            "value": 5.652311164803127,
+            "unit": "Evt/s"
           }
         ]
       }
