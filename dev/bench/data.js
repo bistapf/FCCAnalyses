621f6e087b9eaa00e42d5971f4d86ee69a466858
github-action-benchmark <github@users.noreply.github.com>
2022-11-29 23:20:56 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c479cf909ea3f700de706dff1157c5b7014ae37d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669739255823,
+  "lastUpdate": 1669764056197,
   "repoUrl": "https://github.com/bistapf/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -1368,6 +1368,58 @@ window.BENCHMARK_DATA = {
             "name": "prod | Events processed per second: examples/FCCee/test",
             "value": 8.631156087547986,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "79460851+bistapf@users.noreply.github.com",
+            "name": "bistapf",
+            "username": "bistapf"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c479cf909ea3f700de706dff1157c5b7014ae37d",
+          "message": "Merge pull request #3 from tmadlener/fcchh_tutorial\n\nInstall the FastJet addons headers to work around #227",
+          "timestamp": "2022-11-30T00:13:13+01:00",
+          "tree_id": "d2e9e2a687f7f981b58ef6a4431ff1c8d44e8466",
+          "url": "https://github.com/bistapf/FCCAnalyses/commit/c479cf909ea3f700de706dff1157c5b7014ae37d"
+        },
+        "date": 1669764054980,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: case-studies/analysis_example/scripts",
+            "value": 10.079339265823364,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 11.391075372695923,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 32.226349115371704,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test",
+            "value": 19.82374119758606,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
