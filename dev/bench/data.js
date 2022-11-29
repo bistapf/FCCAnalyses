2d977df618ee3dc98e052dc2d1e7bfa4acab74b3
github-action-benchmark <github@users.noreply.github.com>
2022-11-29 23:20:57 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c479cf909ea3f700de706dff1157c5b7014ae37d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669764056197,
+  "lastUpdate": 1669764057619,
   "repoUrl": "https://github.com/bistapf/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -1420,6 +1420,50 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/test"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "79460851+bistapf@users.noreply.github.com",
+            "name": "bistapf",
+            "username": "bistapf"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c479cf909ea3f700de706dff1157c5b7014ae37d",
+          "message": "Merge pull request #3 from tmadlener/fcchh_tutorial\n\nInstall the FastJet addons headers to work around #227",
+          "timestamp": "2022-11-30T00:13:13+01:00",
+          "tree_id": "d2e9e2a687f7f981b58ef6a4431ff1c8d44e8466",
+          "url": "https://github.com/bistapf/FCCAnalyses/commit/c479cf909ea3f700de706dff1157c5b7014ae37d"
+        },
+        "date": 1669764057249,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts",
+            "value": 9.921285251214448,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 8.77880241576639,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 3.1030508495391684,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test",
+            "value": 5.044456493014398,
+            "unit": "Evt/s"
           }
         ]
       }
