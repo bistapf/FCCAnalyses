cfd139f33714cf69b42f40dda8f1c359d4542839
bistapf <bistapf@users.noreply.github.com>
2022-12-06 14:54:45 +0000

    deploy: 673c6af21e88434e82131bbe9f0ef14303c1117d

--- doc/latest/search/searchdata.js
+++ doc/latest/search/searchdata.js
@@ -1,13 +1,13 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghijlmnoprstuvwx~",
+  0: "_abcdefghijlmnoprstuvwxz~",
   1: "abcefglmoprstvw",
-  2: "cfjo",
+  2: "acfjlo",
   3: "acdejlmorsuvw",
-  4: "abcdefghijlmoprstuvwx~",
+  4: "abcdefghijlmoprstuvwxz~",
   5: "_bcdefgijlmnoprsuv",
   6: "cftv",
-  7: "_t",
+  7: "_at",
   8: "f"
 };
 
