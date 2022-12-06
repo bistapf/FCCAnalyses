d73ffb18436056a1b16240b166293c7eacc0347f
github-action-benchmark <github@users.noreply.github.com>
2022-12-06 14:58:35 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 76ca1d4fc1c71e4756b98850548d9650279c20ae

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670338687788,
+  "lastUpdate": 1670338715786,
   "repoUrl": "https://github.com/bistapf/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -1840,6 +1840,56 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/test",
             "value": 8.120785784163997,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bstapf@nikhef.nl",
+            "name": "Birgit Stapf"
+          },
+          "committer": {
+            "email": "bstapf@nikhef.nl",
+            "name": "Birgit Stapf"
+          },
+          "distinct": true,
+          "id": "76ca1d4fc1c71e4756b98850548d9650279c20ae",
+          "message": "updating master and adding MC particles",
+          "timestamp": "2022-12-06T15:49:22+01:00",
+          "tree_id": "c27e582e404697d00b3f29d7e2b9675739b636d0",
+          "url": "https://github.com/bistapf/FCCAnalyses/commit/76ca1d4fc1c71e4756b98850548d9650279c20ae"
+        },
+        "date": 1670338714246,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Time spent running the analysis: case-studies/analysis_example/scripts",
+            "value": 9.685826539993286,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 11.943763494491577,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 15.300997734069824,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          },
+          {
+            "name": "nightly | Time spent running the analysis: examples/FCCee/test",
+            "value": 11.092407941818237,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test"
           }
         ]
       }
