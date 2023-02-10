5fb9dd7defc329e4a9762e24d48b503529677e97
Birgit Stapf <bistapf@desy.de>
2023-02-10 10:12:05 +0100

    add histograms per bin of var1 to resolutionplotter

--- run/Delphescard_validation/bbzz_4l_eff_rel_checks.py
+++ run/Delphescard_validation/bbzz_4l_eff_rel_checks.py
@@ -7,6 +7,7 @@ import os
 import matplotlib.pyplot as plt
 from array import array
 import helpers 
+import resolutionPlotter
 
 ROOT.gROOT.SetBatch()
 ROOT.gStyle.SetOptTitle(0)
@@ -54,7 +55,7 @@ def validate_file(input_filepath, out_dir_base):
 #version of this function that loops over vector of 4 leptons matched to truth, and adds the resulting resolution histograms for more statistics
 #feed with rdf where 4 leptons are already required!!
 #eta edges should be a tuple of minimum, maximum
-def check_lep_res_per_eta_bin_4l(input_rdf, eta_edges, E_edges, filebasename, out_dir_base, iso):
+def check_lep_energy_res_per_eta_bin_4l(input_rdf, eta_edges, E_edges, filebasename, out_dir_base, iso):
 
 	model = ROOT.RDF.TH1DModel("resolution_model_hist", "resolution_model_hist", 40, -0.05, 0.05)
 
@@ -126,7 +127,7 @@ def check_lepton_resolutions(flavour, input_filepath, out_dir_base):
 		rdf_true_4m = rdf_true_4l.Filter("abs(pdgID_truth_leps_from_HZZ[0]) == 13 && abs(pdgID_truth_leps_from_HZZ[1]) == 13 && abs(pdgID_truth_leps_from_HZZ[2]) == 13 && abs(pdgID_truth_leps_from_HZZ[3]) == 13 ")
 		rdf_true_4l_recomatched = rdf_true_4m.Filter("n_truthmatched_leps_from_HZZ_noiso == 4" )
 
-	#first only in bins of eta:
+	#first check: energy resolutions vs E in bins of eta
 	list_of_hists =[]
 	eta_edges = [0., 2.5, 4., 6]
 	E_edges = [0., 50., 100., 200.] #TODO: CHECK IF CAN BE FINER
@@ -135,12 +136,13 @@ def check_lepton_resolutions(flavour, input_filepath, out_dir_base):
 		hist_title = "{} < |#eta| < {}".format(eta_edges[i_eta_edge], eta_edges[i_eta_edge+1])
 		hist_filebase = "{}_E_resolution_eta_bin{}".format(flavour, i_eta_edge)
 		eta_edges_tuple = [eta_edges[i_eta_edge], eta_edges[i_eta_edge+1]]
-		hist_eta_bin = check_lep_res_per_eta_bin_4l(rdf_true_4l_recomatched, eta_edges_tuple, E_edges, hist_filebase, out_dir_base, False)
+		hist_eta_bin = check_lep_energy_res_per_eta_bin_4l(rdf_true_4l_recomatched, eta_edges_tuple, E_edges, hist_filebase, out_dir_base, False)
 		hist_eta_bin.SetTitle(hist_title)
 		list_of_hists.append(hist_eta_bin)
 
-	#TO EDIT
-	helpers.plot_list_of_hists(list_of_hists, "{}_E_resolution".format(flavour), out_dir_base, "E_{truth} in GeV", "#sigma(E)/E", file_format="png")
+	helpers.plot_list_of_hists(list_of_hists, "{}_E_resolution".format(flavour), out_dir_base, "E_{truth} in GeV", "{} : #sigma(E)/E".format(flavour), file_format="png")
+
+	#second check: energy resolutions vs eta in bins of E
 
 
 if __name__ == "__main__":
@@ -151,10 +153,21 @@ if __name__ == "__main__":
 	parser.add_argument('--lep', '-l', metavar="lepton", dest="lep", required=True, help="electron or muon")
 	args = parser.parse_args()
 
-	check_lepton_resolutions(args.lep, args.inPath, args.outDir)
+	# check_lepton_resolutions(args.lep, args.inPath, args.outDir)
+
+	#test the class method:
+	res_plotter = resolutionPlotter.ResolutionPlotter(args.inPath, args.outDir, args.lep)
+	res_plotter.filter_input_rdf("n_truth_leps_from_HZZ == 4") #use only a subset of events
+
+	#start with energy resolutions vs E in bins of eta :
+	res_plotter.set_binning("eta_truth_leps_from_HZZ", [0., 2.5, 4., 6], "|#eta_{truth}|", "E_truth_leps_from_HZZ", [0., 50., 100., 200.], "E_{truth} in GeV")
+	res_plotter.plot_resolution_histograms("E_res_eta_bins_vs_E", "E_truthmatched_leps_from_HZZ_noiso", "E_truth_leps_from_HZZ", "#sigmaE/E",  4)
+
+	# res_plotter.build_cutstring_bin(["eta_truth_leps_from_HZZ", "E_truth_leps_from_HZZ"], list_bin_edge_pairs, object_index)
 
 	# validate_file(args.inPath, args.outDir)
 
 # checking tester file:
 # python bbzz_4l_eff_rel_checks.py -i /eos/user/b/bistapf/FCChh_EvtGen/FCCAnalysis_ntuples_noIso/FCChh_EvtGen_pwp8_pp_hh_5f_hhbbZZ_4l_e_mu_excl.root -o ./bbzz_4l_checks/
 # python bbzz_4l_eff_rel_checks.py -i /eos/user/b/bistapf/FCChh_EvtGen/FCCAnalysis_ntuples_noIso/pwp8_pp_hh_lambda100_5f_hhbbzz_4l/ -o ./bbzz_4l_checks/ -l muon
+# python bbzz_4l_eff_rel_checks.py -i /eos/user/b/bistapf/FCChh_EvtGen/FCCAnalysis_ntuples_noIso/pwp8_pp_hh_lambda100_5f_hhbbzz_4l/ -o ./bbzz_4l_checks_class/ -l muon
--- run/Delphescard_validation/resolutionPlotter.py
+++ run/Delphescard_validation/resolutionPlotter.py
@@ -1,5 +1,9 @@
 import ROOT
 import os 
+from array import array
+
+import helpers
+
 
 class ResolutionPlotter:
 	"""Generic class to make plots of various resolutions, in bins of other variables"""
@@ -9,10 +13,20 @@ class ResolutionPlotter:
 
 		self.object_name = object_name
 
+		if not os.path.exists(output_dir):
+			os.mkdir(output_dir)
+
+		self.output_dir = output_dir
+
 		self.rdf = self.get_rdf(input_path)
 
 		if not self.rdf:
 			raiseException("Error in initializing ResolutionPlotter - no rdf created.")
+
+		#setup for the resolution histograms:
+		self.res_model_nbins = 40
+		self.res_model_xmin = -0.05
+		self.res_model_xmax = 0.05
 
 	# Open the requested file(s) into the rdf
 	def get_rdf(self, input_path):
@@ -28,5 +42,83 @@ class ResolutionPlotter:
 
 		return rdf
 
+	#apply some cut to the rdf before calculating resolutions - overwrites the original
+	def filter_input_rdf(self, cutstring):
 
-	# def get_resolution_in_bin(input_rdf, )
+		filtered_rdf = self.rdf.Filter(cutstring)
+
+		self.rdf = filtered_rdf
+
+	def set_binning(self, primary_var_name, primary_var_bin_edges, primary_var_label, secondary_var_name, secondary_var_bin_edges, secondary_var_label):
+
+		self.var1 = primary_var_name
+		self.var1_edges = primary_var_bin_edges
+		self.var1_label = primary_var_label
+		self.var2 = secondary_var_name
+		self.var2_edges = secondary_var_bin_edges
+		self.var2_label = secondary_var_label
+
+	def build_cutstring_bin(self, var_name, var_edges, object_index):
+
+		if "eta" in var_name:
+			cutstring = "abs({}[{}]) > {:.2f} && abs({}[{}]) <= {:.2f}".format(var_name, object_index, var_edges[0], var_name, object_index, var_edges[1])
+		else:
+			cutstring = "{}[{}] > {:.2f} && {}[{}] <= {:.2f}".format(var_name, object_index, var_edges[0], var_name, object_index, var_edges[1])
+
+		return cutstring
+
+	def merge_cutstrings(self, cutstring1, cutstring2):
+		return "{} && {}".format(cutstring1, cutstring2)
+
+	def plot_resolution_histograms(self, hist_base_name, reco_var_name, truth_var_name, res_label, number_of_objects):
+
+		if not self.var1 or not self.var1_edges or not self.var2 or not self.var2_edges:
+			raiseException("Error in plotting resolution histograms - no binning info provided, please call set_binning(primary_var_name, primary_var_bin_edges, secondary_var_name, secondary_var_bin_edges) before running this.") 
+
+		#loop over the primary var and then make histograms w.r.t secondary var:
+		for var1_edge in range(len(self.var1_edges)-1):
+			#histogram vs var2
+			hist_binEdges = array("d", self.var2_edges)
+			hist_nBins = len(self.var2_edges)-1
+			hist_vs_var2_name = "{}_var1_bin_{}".format(hist_base_name, var1_edge)
+			hist_vs_var2 = ROOT.TH1D(hist_vs_var2_name, hist_vs_var2_name, hist_nBins, hist_binEdges)
+			hist_vs_var2_title = "{:.2f} < {} < {:.2f} ".format(self.var1_edges[var1_edge], self.var1_label, self.var1_edges[var1_edge+1])
+			hist_vs_var2.SetTitle(hist_vs_var2_title)
+
+			for var2_edge in range(len(self.var2_edges)-1):
+				res_hist_bin = ROOT.TH1D("res_hist_bin", "res_hist_bin", 40, -0.05, 0.05)
+
+				# make sure we loop over all the objects per event that we calculate the resolution for, e.g. in bbZZ(4l) we take the res from all 4 leps
+				for object_i in range(number_of_objects):
+
+					model = ROOT.RDF.TH1DModel("resolution_model_hist", "resolution_model_hist", self.res_model_nbins, self.res_model_xmin, self.res_model_xmax)
+
+					cutstring_var1 = self.build_cutstring_bin(self.var1, (self.var1_edges[var1_edge], self.var1_edges[var1_edge+1]), object_i)
+					cutstring_var2 = self.build_cutstring_bin(self.var2, (self.var2_edges[var2_edge], self.var2_edges[var2_edge+1]), object_i)
+					cutstring_bin = self.merge_cutstrings(cutstring_var1, cutstring_var2)
+					print("Calculating for bin:", cutstring_bin)
+				
+					rdf_bin = self.rdf.Filter(cutstring_bin)
+					# print("Number of events in bin:", rdf_bin.Count().GetValue())
+
+					#now define the relative resolution ! of (reco-truth)/truth:you dont 
+					rel_res_def_string = "({}[{}] - {}[{}])/{}[{}]".format(reco_var_name, object_i, truth_var_name, object_i, truth_var_name, object_i)
+					rdf_resolution = rdf_bin.Define("resolution", rel_res_def_string)
+					tmp_hist = rdf_resolution.Histo1D(model, "resolution").GetValue()   
+					res_hist_bin.Add(tmp_hist)
+
+				histfilename = "{}_{}_{}_x_{}".format(self.object_name, hist_base_name, var1_edge, var2_edge)  
+				gaus_pars = helpers.plot_single_hist(res_hist_bin, histfilename, self.output_dir, "#sigma(E)/E", "Events", do_gauss_fit=True, colour_code=38, file_format="png")
+
+				if gaus_pars:
+					hist_vs_var2.SetBinContent(var2_edge+1, gaus_pars[2])
+					hist_vs_var2.SetBinError(var2_edge+1, gaus_pars[3])
+
+				helpers.plot_single_hist(hist_vs_var2, hist_vs_var2_name, self.output_dir, self.var2_label, res_label, do_gauss_fit=False, colour_code=38, file_format="png")
+
+
+
+
+	# def get_resolution_in_bin(self, reco_var_name, truth_var_name,  number_of_objects):
+
+	# 	model = ROOT.RDF.TH1DModel("resolution_model_hist", "resolution_model_hist", self.res_model_nbins, self.res_model_xmin, self.res_model_xmax)
