6ad2b572c16a2253b64e521a559560db8ee8eae6
Birgit Stapf <bistapf@desy.de>
2023-07-24 16:02:29 +0200

    point common_defaults to FCC-hh specific directory (temp)

--- config/common_defaults.py
+++ config/common_defaults.py
@@ -1,2 +1,4 @@
 #deffccdicts     = "/afs/cern.ch/work/f/fccsw/public/FCCDicts/"
-deffccdicts     = "/cvmfs/fcc.cern.ch/FCCDicts/"
+# deffccdicts     = "/cvmfs/fcc.cern.ch/FCCDicts/"
+#temp overwrite for FCC-hh
+deffccdicts     = "/eos/experiment/fcc/hh/utils/FCCDicts/"
